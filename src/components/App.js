c0f7f5bd0d38c0f12b2d6c7aa7327ed2e2996fb1
Frank <frank7300228@gmail.com>
2018-12-05 17:54:16 +0800

    Finish 16-196

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,20 @@ import React from "react"
 import { BrowserRouter, Route } from "react-router-dom"
 
 const PageOne = () => {
-  return <div>Page1</div>
+  return (
+    <div>
+      Page1
+      <a href="/pagetwo">Navigate to Page Two</a>
+    </div>
+  )
 }
 
 const PageTwo = () => {
-  return <div>Page2</div>
+  return (
+    <div>
+      Page2<a href="/">Navigate to Page One</a>
+    </div>
+  )
 }
 
 const App = () => {
